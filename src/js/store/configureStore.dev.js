7868af196642e7d0dfa8d10430f32e102ff46d87
liulang <1482666460@qq.com>
2017-07-23 09:46:18 +0800

    redux-observable

--- src/js/store/configureStore.dev.js
+++ src/js/store/configureStore.dev.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+//import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/root.js';
 import DevTools from '../containers/DevTools.js';
+import rootEpic from '../reducers/index.epics.js';
+import { createEpicMiddleware } from 'redux-observable';
 
+const epicMiddleware = createEpicMiddleware(rootEpic);
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
     compose(
-      applyMiddleware(thunkMiddleware),
+      applyMiddleware(epicMiddleware),
       DevTools.instrument()
       )
   );
