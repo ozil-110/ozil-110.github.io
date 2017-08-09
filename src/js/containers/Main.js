7064e828524927a90c2e2c7f8e0d0692ccfe75fe
刘浪 <1482666460@qq.com>
2017-08-09 11:57:18 +0800

    Update Main.js

--- src/js/containers/Main.js
+++ src/js/containers/Main.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.export = require('./App.js');
+    module.exports = require('./App.js');
 } else {
     module.exports = require('./App.dev.js');
 }
