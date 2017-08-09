6d18f9d8f6715412d1e485ae0246a8fe91353301
刘浪 <1482666460@qq.com>
2017-08-09 11:54:01 +0800

    Update back.js

--- src/js/constants/back.js
+++ src/js/constants/back.js
@@ -3,29 +3,33 @@ var scrollEle = document.body,
     rate = 0.8,
     time = 20;
 setTimeout(function() {
-        var backImg = document.querySelector('.back');
+        var backImg = document.querySelector('.back');  
         backImg.onclick = function() {
-        var moveInterval = setInterval(moveScroll,time);
-        function moveScroll() {
-            var scrollTop = scrollEle.scrollTop;
-            if (scrollTop === 0) {
-                clearInterval(moveInterval);
-                return ;
+            var moveInterval = setInterval(moveScroll,time);
+            function moveScroll() {
+                var scrollTop = scrollEle.scrollTop;
+                if (scrollTop === 0) {
+                    clearInterval(moveInterval);
+                    return ;
+                }
+                scrollEle.scrollTop = scrollTop * rate;
             }
-            scrollEle.scrollTop = scrollTop * rate;
+        };
+      
+       
+        var scrollFunction = function(e) {
+            e = e || window.evevt;
+            var display = backImg.style.display;
+            if (scrollEle.scrollTop>20) {
+                if (display !== "block") {
+                    backImg.style.display = "block";
+                }
+            } else {
+                if (display !== "none") {
+                    backImg.style.display = "none";
+                }
+            }
         }
-    };
-        window.onscroll = function() {
-        var display = backImg.style.display;
-        if (scrollEle.scrollTop>20) {
-            if (display !== "block") {
-                backImg.style.display = "block";
-            }
-        } else {
-            if (display !== "none") {
-                backImg.style.display = "none";
-            }
-        }
-    }    
+        window.onscroll=document.onscroll = scrollFunction;  
 },1000)    
      
